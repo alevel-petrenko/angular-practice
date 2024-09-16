e45e9da831f26f0f822e543ce9adc0e0ce72523f
Anton Petrenko <c-Anton.Petrenko@skykick.com>
2024-09-16 11:02:18 +0300

    using host and parameters in directive

--- purchaseapp/src/app/child/child.component.ts
+++ purchaseapp/src/app/child/child.component.ts
@@ -1,19 +1,15 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { TestComponent } from '../test.component'
 import { HighlightDirective } from '../directive/appHighlight.directive'
+import { BoldDirective } from '../directive/bold.directive'
 
 @Component({
     selector: 'child-comp',
     standalone: true,
-    imports: [FormsModule, TestComponent, HighlightDirective],
+    imports: [FormsModule, TestComponent, HighlightDirective, BoldDirective],
     styles: `h2 {font-size:30px; color:red;}`,
-    template: `<ng-content></ng-content>
-        <my-app #pageCount [name]="userName"></my-app>
-        <button (click)="pageCount.next()">Next page</button>
-        <button (click)="pageCount.previous()">Previous page</button>
-        <h2 appHighlight>Here is what I have: {{_shittyText}}</h2>
-        <input [ngModel]="userName" (ngModelChange)="onNameChange($event)" />`
+    templateUrl: './child.component.html'
 })
 export class ChildComponent {
     _shittyText: string = "";
