b48681f9a7bbff2f443019bb9065b6b850745b25
Anton Petrenko <c-Anton.Petrenko@skykick.com>
2024-11-06 11:55:45 +0200

    added custom CoursesService for http calls

--- udemy-3-services/src/app/app.component.ts
+++ udemy-3-services/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {Course} from './model/course';
-import {Observable} from 'rxjs';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { Course } from './model/course';
+import { Observable } from 'rxjs';
+import { CoursesService } from './services/courses.service';
 
 @Component({
   selector: 'app-root',
@@ -12,20 +12,10 @@ export class AppComponent implements OnInit {
   public allCourses: Course[];
   public allCourses$: Observable<Course[]>;
 
-  constructor(private http: HttpClient) {
+  constructor(private coursesService: CoursesService) {
   }
 
   ngOnInit() {
-    const params = new HttpParams()
-      .set('page', '1')
-      .set('pageSize', '10');
-
-    this.allCourses$ = this.http.get<Course[]>('/api/courses', { params });
-
-    this.http.get<Course[]>('/api/courses', { params })
-      .subscribe(
-        (courses:Course[]) => this.allCourses = courses,
-        (error) => console.error('Failed to fetch courses:', error)
-      );
+    this.allCourses$ = this.coursesService.loadCourses();
   }
 }
