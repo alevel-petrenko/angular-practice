d56fdaf6bda3e45fb2e9d5264f062bd365733c7f
Anton Petrenko <c-Anton.Petrenko@skykick.com>
2025-01-02 12:04:34 +0200

    dependencies between signals and effect()

--- udemy-3-services/src/app/signals-example/signals-example.component.ts
+++ udemy-3-services/src/app/signals-example/signals-example.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'signals-example',
@@ -8,9 +8,19 @@ import { Component, computed, signal } from '@angular/core';
   styleUrl: './signals-example.component.css'
 })
 export class SignalsExampleComponent {
+  constructor(){
+    effect(() => {
+      const counterValue = this.counter();
+      const multipleCounterValue = this.tenXCounter();
+
+      console.log(`Update value: counter is ${counterValue}, multiple is ${multipleCounterValue}.`)
+    })
+  }
+
   counter = signal(0);
 
   tenXCounter = computed(() => {
+    // dependencies for derived signals are created during the last `computed` execution
     const counter = this.counter();
     return counter * 10; 
   })
