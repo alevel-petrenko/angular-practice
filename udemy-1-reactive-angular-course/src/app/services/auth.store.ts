ddfbcdf58a95aba17ab5ac8015f2ccca274c8770
Anton Petrenko <c-Anton.Petrenko@skykick.com>
2025-02-13 12:28:07 +0200

    added storing user in local storage

--- udemy-1-reactive-angular-course/src/app/services/auth.store.ts
+++ udemy-1-reactive-angular-course/src/app/services/auth.store.ts
@@ -4,16 +4,22 @@ import { IUser } from "../model/user";
 import { map, shareReplay, tap } from "rxjs/operators";
 import { HttpClient } from "@angular/common/http";
 
+const AUTH_USER = 'auth_private_user';
+
 @Injectable({
     providedIn: 'root'
 })
 export class AuthStore {
-    private subject = new BehaviorSubject(null);
+    private subject = new BehaviorSubject<IUser>(null);
     user$: Observable<IUser> = this.subject.asObservable();
     isLoggedIn$: Observable<boolean>;
     isLoggedOut$: Observable<boolean>;
 
     constructor(private http: HttpClient) {
+        const userAsString = localStorage.getItem(AUTH_USER);
+        if (userAsString)
+            this.subject.next(JSON.parse(userAsString));
+
         this.isLoggedIn$ = this.user$
             .pipe(
                 map(user => !!user)
@@ -27,12 +33,16 @@ export class AuthStore {
     login(email: string, password: string): Observable<IUser> {
         return this.http.post<IUser>('/api/login', { email, password })
             .pipe(
-                tap(user => this.subject.next(user)),
+                tap(user => {
+                    this.subject.next(user);
+                    localStorage.setItem(AUTH_USER, JSON.stringify(user));
+                }),
                 shareReplay()
             );
     }
 
     logout() {
         this.subject.next(null);
+        localStorage.removeItem(AUTH_USER);
     }
 }
