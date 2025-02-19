5c2dfa48927bf5c95996b1fc05052fcbf66d6e61
Anton Petrenko <c-Anton.Petrenko@skykick.com>
2025-02-19 11:29:25 +0200

    Added dispaying course's details and lessons for particular course

--- udemy-1-reactive-angular-course/src/app/course/course.component.ts
+++ udemy-1-reactive-angular-course/src/app/course/course.component.ts
@@ -1,55 +1,30 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {Course} from '../model/course';
-import {
-  debounceTime,
-  distinctUntilChanged,
-  startWith,
-  tap,
-  delay,
-  map,
-  concatMap,
-  switchMap,
-  withLatestFrom,
-  concatAll, shareReplay, catchError
-} from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat, throwError} from 'rxjs';
-import {Lesson} from '../model/lesson';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Course } from '../model/course';
+import { Lesson } from '../model/lesson';
+import { Observable } from 'rxjs';
+import { CoursesService } from '../services/courses.service';
 
 
 @Component({
-    selector: 'course',
-    templateUrl: './course.component.html',
-    styleUrls: ['./course.component.css'],
-    standalone: false
+  selector: 'course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.css'],
+  standalone: false
 })
 export class CourseComponent implements OnInit {
 
-  course: Course;
+  course$: Observable<Course>;
+  lessons$: Observable<Lesson[]>;
 
-  lessons: Lesson[];
-
-  constructor(private route: ActivatedRoute) {
-
-
-  }
+  constructor(private route: ActivatedRoute,
+    private coursesService: CoursesService
+  ) { }
 
   ngOnInit() {
+    const courseId = this.route.snapshot.paramMap.get('courseId');
 
-
-
+    this.course$ = this.coursesService.loadCourseById(courseId);
+    this.lessons$ = this.coursesService.loadAllCourseLessons(courseId);
   }
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
